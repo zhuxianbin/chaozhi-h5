b8df1221e6abe8f2e62a5d1d9dba96ba42d57709
zhuxianbin <zhuxianbin@evcoming.com>
2018-03-22 10:29:34 +0800

    11222

--- src/app.vue
+++ src/app.vue
@@ -2,9 +2,9 @@
   <div>
     <mt-header v-if='currentRouter!="login"' class='layout-header' fixed title="">
       <mt-button v-if="showBack" slot="left" @click.native='$router.back()' icon="back"></mt-button>
-      <div slot="right">
+      <!-- <div slot="right">
         <mt-button style='width:100%;' @click.native='doCallServer' plain size="small">立即咨询</mt-button>
-      </div>
+      </div> -->
     </mt-header>
     <router-view></router-view>
     <mt-tabbar v-if='currentRouter!="login"' :fixed="true" v-model="tabActive">
@@ -13,6 +13,37 @@
         {{item.name}}
       </mt-tab-item>
     </mt-tabbar>
+
+    <mt-popup
+      v-model="popupASK"
+      popup-transition="popup-fade">
+      <div class="ui-alert">
+        <div class="ui-alert-title">支付提醒</div>
+        <div class="ui-alert-body">
+          <div flex>
+            <div flex-box="0">
+              <i style='font-size:2rem;margin-right:1rem;' class="iconfont icon-tishi t-red"></i>
+            </div>
+            <div flex-box="1">
+              <div class="mb-10">支付失败,请点击重新支付</div>
+              <div class="mb-20">订单信息:剑桥英语第一次</div>
+            </div>
+          </div>
+          <div flex class="mb-10">
+            <div flex-box="1" style='margin-right:.5rem;'>
+              <mt-button type="danger" style='width:100%;'>继续支付</mt-button>
+            </div>
+            <div flex-box="1">
+              <mt-button type="default" style='width:100%;'>返回商城</mt-button>
+            </div>
+          </div>
+          
+          <div class="t-center t-xs">
+              支付遇到问题:请联系010-5165777
+          </div>
+        </div>
+      </div>
+    </mt-popup>
   </div>
 </template>
 
@@ -45,7 +76,8 @@ export default {
         }
       ],
       currentRouter: "index",
-      tabActive: "index"
+      tabActive: "index",
+      popupASK:false,
     };
   },
   watch: {
