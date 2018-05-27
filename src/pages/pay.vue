456824867c8fc1a61f1215dfe5b01b74f4fa6884
zhuxianbin <zhuxianbin@evcoming.com>
2018-05-27 16:36:18 +0800

    微信支付长按问题临时解决方案

--- src/pages/pay.vue
+++ src/pages/pay.vue
@@ -24,10 +24,13 @@
       <mt-tab-item id="wechat">微信</mt-tab-item>
       <mt-tab-item id="alipay">支付宝</mt-tab-item>
     </mt-navbar>
-    <div v-if="!!qrcode" style='padding:1rem;background:#fff;'>
+    <div v-if="!!qrcode" style='padding:1rem 2rem;background:#fff;'>
       <div class="mb-20 t-center">
         <div class="t-xs mb-10">请扫二维码进行支付</div>
-        <img :src='qrcode' style='width:50vw;height:50vw;max-width:300px;max-height:300px;' /> 
+        <img :src='qrcode' style='width:50vw;height:50vw;max-width:300px;max-height:300px;' />
+        <div class="t-xs">*如遇到支付问题,请截屏此页面,从微信扫一扫界面,打开相册选择截屏图片进行支付</div>
+        <!-- <div class="mb-20">或</div>
+        <mt-button @click.native='jumpWechatPay' size="large" type="primary">{{qrtext}}</mt-button> -->
       </div>
     </div>
     <div v-if='payType=="alipay"' class="t-center" style='padding:30px;'>
@@ -65,7 +68,8 @@ export default {
           value: "alipay"
         }
       ],
-      qrcode: ""
+      qrcode: "",
+      qrtext: ""
     };
   },
   watch: {
@@ -105,6 +109,7 @@ export default {
         this.payResult = data;
         this.getPayResult(data.token);
         this.qrcode = "";
+        this.qrtext = data.qrtext;
         data.qrtext &&
           QRCode.toDataURL(data.qrtext, { errorCorrectionLevel: "H" }).then(
             url => {
@@ -134,6 +139,9 @@ export default {
       document.getElementById("alipaysubmit").target = "_blank";
       document.forms["alipaysubmit"].submit();
     }
+    // jumpWechatPay() {
+    //   window.location.href = this.qrtext;
+    // }
   },
   mounted() {
     let { id: product_id } = this.$route.query;
