d3cf594ec6d511b3a7f6b0d41bd3ce1613ba552a
zhuxianbin <zhuxian123@qq.com>
2018-06-03 10:11:57 +0800

    11111111111

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 export const baseUrl =
   //"http://101.201.222.8:8081" ||
-  //"http://dev-aci-api.chaozhiedu.com" ||
+  "http://dev-aci-api.chaozhiedu.com" ||
   "http://aci-api.chaozhiedu.com";
 
 export const weixinAuth =
-  //"http://dev-aci-api.chaozhiedu.com" ||
+  "http://dev-aci-api.chaozhiedu.com" ||
   "http://aci-api.chaozhiedu.com";
--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -87,13 +87,17 @@ export default {
           let href = `http://test-aci-api.chaozhiedu.com/api/weixinauth?token=${token}&url=${redirect_uri}`;
           window.location.href = href;
         },
-        weixinPay(config,callback) {
+        weixinPay(config, callback) {
+          if (!config) {
+            this.$toast("config错误");
+            return false;
+          }
           function onBridgeReady() {
             WeixinJSBridge.invoke(
               'getBrandWCPayRequest', config,
               function (res) {
                 if (res.err_msg == "get_brand_wcpay_request:ok") {
-                  callback&&callback();
+                  callback && callback();
                 }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
               }
             );
