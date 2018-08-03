32a6586cabc47abe8aee414dd25d1ba8df57da2a
zhuxianbin <zhuxian123@qq.com>
2018-08-03 19:55:43 +0800

    321

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -3,6 +3,11 @@
 import storage from "./storage";
 import api from "./api";
 import tools from "./tools";
+import Cookies from "js-cookie"
+import {
+  getToken,
+  removeToken
+} from "./auth";
 
 const $cookie = {
   //设置cookie
@@ -33,7 +38,7 @@ export default {
   install(Vue) {
     Vue.mixin({
       data() {
-        let { token } = this.$storage.get("userToken");
+        let token = getToken();
         return {
           Token: token
         };
@@ -50,7 +55,7 @@ export default {
       },
       methods: {
         doLogout() {
-          storage.remove("userToken");
+          removeToken();
           //$cookie.clearCookie("userToken", "", -1);
           //$.cookie("userToken", "", -1);
           //console.log(this.$router,1111);
@@ -68,12 +73,12 @@ export default {
         initWeiXin(jsApiList = [], callback) {
           api.getWeiXinConfig().then(() => {
             wx.config({
-              beta: true,// 必须这么写，否则wx.invoke调用形式的jsapi会有问题
+              beta: true, // 必须这么写，否则wx.invoke调用形式的jsapi会有问题
               debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
               appId: '', // 必填，企业微信的corpID
               timestamp: 0, // 必填，生成签名的时间戳
               nonceStr: '', // 必填，生成签名的随机串
-              signature: '',// 必填，签名，见附录1
+              signature: '', // 必填，签名，见附录1
               jsApiList: jsApiList // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
             wx.ready(() => {
@@ -82,7 +87,7 @@ export default {
           });
         },
         bindWeiXinOpenId(url) {
-          let { token } = storage.get("userToken");
+          const token = getToken();
           let redirect_uri = encodeURIComponent(url);
           let href = `http://aci-api.chaozhiedu.com/api/weixinauth?token=${token}&url=${redirect_uri}`;
           window.location.href = href;
@@ -92,13 +97,14 @@ export default {
             this.$toast("config错误");
             return false;
           }
+
           function onBridgeReady() {
             WeixinJSBridge.invoke(
               'getBrandWCPayRequest', config,
               function (res) {
                 if (res.err_msg == "get_brand_wcpay_request:ok") {
                   callback && callback();
-                }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+                } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
               }
             );
           }
